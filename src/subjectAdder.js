6abbd3c2441f0c5307e217a1d55a677db5ab6e7c
Gunn <gunn@Gunns-MacBook-Pro.local>
2018-03-18 15:32:02 +0700

    add some css

--- src/subjectAdder.js
+++ src/subjectAdder.js
@@ -1,36 +1,37 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 
-class SubjectAdder extends Component{
-    constructor(props){
+class SubjectAdder extends Component {
+    constructor(props) {
         super(props);
         this.state = {
-            subjectname:'',
-            
+            subjectname: '',
+
         }
         this.addTask = this.addTask.bind(this);
     }
 
-    addTask(e){
+    addTask(e) {
         this.setState({
             subjectname: e.target.value
         })
     }
-    
+
     render() {
-        return(
-            <div className="container-fluid row">
-                <div className="input-group form-inline" style={{marginTop:"30px"}}>
-                <input className="form-control col-md-5" onChange={this.addTask} style={{marginLeft:"350px"}} placeholder={"Add New Task . . ."}></input>
-                <br/>
-                <div className="input-group-append">
-                <button className="btn btn-light border " onClick={()=>this.props.Addnewtask(this.state.subjectname)}>
-                    <i className="fas fa-plus"></i>
-                </button>
+        return (
+            <div className="container is-fluid" style={{marginTop:"15px"}}>
+                <div className="field has-addons has-addons-centered">
+                <div className="control">
+                    <input className="input" style={{width:"400px",textAlign:"right"}}onChange={this.addTask} placeholder={"Add New Task . . ."}></input>
+                </div>
+                <div className="control">
+                    <a className="button" onClick={() => this.props.Addnewtask(this.state.subjectname)}>
+                        <i className="fas fa-plus"></i>
+                    </a>
                 </div>
                 </div>
             </div>
         );
     }
-}   
+}
 
 export default SubjectAdder;
--- src/tasklist.js
+++ src/tasklist.js
@@ -1,23 +1,26 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 
-class Tasklist extends Component{
+class Tasklist extends Component {
 
-    render(){
-        return(
-            <div className="list" style={{marginTop:"30px"}}>
+    render() {
+        return (
+            <div className="list" style={{ marginTop: "30px" }}>
                 <ul>
-                    {this.props.Task.map((subject,id)=>{
-                        return(
-                            <div className="container" key={id}>
-                            <div className="form-group form-inline">
-                            <li style={{paddingTop:"6px"}}>
-                                {subject}
-                             </li>
-                            {'  '}
-                            <button className="btn btn-danger"onClick={()=>this.props.removeTask(id)}>
-                                <i class="fas fa-times" style={{color:"white"}}></i>
-                            </button>
-                            </div>
+                    {this.props.Task.map((subject, id) => {
+                        return (
+                            <div className="container is-fluid" key={id}>
+                                <article className="message" style={{ width: "460px" ,marginTop:"10px"}}>
+                                    <div className="message-header">
+                                        <p>Task {id + 1}</p>
+                                    </div>
+                                    <div className="message-body">
+                                        {subject}
+                                        <a className="button is-danger is-small is-outlined" onClick={() => this.props.removeTask(id)} style={{marginLeft:"20px"}}>
+                                            <i className="fas fa-times" style={{ color: "is-danger" }}></i>
+                                        </a>
+                                    </div>
+                                </article>
+
                             </div>
                         )
                     })}
